1f9bb94d67aa5fcdb1a3857a4f3a159dc01a566e
Lina Bhatia <lina@bhatia.org>
2022-07-27 11:35:42 -0500

    Added user location

--- Za Hunter/ContentView.swift
+++ Za Hunter/ContentView.swift
@@ -9,12 +9,17 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
+    @State private var userTrackingMode: MapUserTrackingMode = .follow
+    @StateObject var locationManager = LocationManager()
     @State private var region = MKCoordinateRegion(
     center: CLLocationCoordinate2D(
         latitude: 42.0558, longitude: -87.6743),
     span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
     var body: some View {
-        Map(coordinateRegion: $region)
+        Map(coordinateRegion: $region,
+            interactionModes: .all,
+        showsUserLocation: true,
+        userTrackingMode: $userTrackingMode)
     }
 }
 
---
+++ Za Hunter/LocationManager.swift
@@ -0,0 +1,19 @@
+//
+//  LocationManager.swift
+//  Za Hunter
+//
+//  Created by Lina Bhatia on 7/27/22.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject{
+    var locationManager = CLLocationManager()
+    override init(){
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
