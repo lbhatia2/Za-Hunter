aea45f287ae7c8dd7f75fc70936423c4b8b1ed31
Lina Bhatia <lina@bhatia.org>
2022-07-27 11:25:41 -0500

    Initial Commit

---
+++ Za Hunter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Za Hunter
+//
+//  Created by Lina Bhatia on 7/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by Lina Bhatia on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
